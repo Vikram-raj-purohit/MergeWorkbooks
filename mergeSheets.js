11c41151fd0521f5a2e7476fead30ed3fabe8221
Vikram-raj-purohit <126610624+Vikram-raj-purohit@users.noreply.github.com>
2023-07-17 10:51:15 +0530

    mergeSheets.js

--- mergeSheets.js
+++ mergeSheets.js
@@ -22,6 +22,14 @@ async function mergeSheets() {
       XLSX.utils.sheet_add_json(mergedSheet, sheetData, { skipHeader: true, origin: -1 });
     });
 
+      processedSheets++;
+
+      // Update progress
+      const progress = Math.round((processedSheets / totalSheets) * 100);
+      updateProgress(progress);
+    });
+
+
     XLSX.utils.book_append_sheet(mergedWorkbook, mergedSheet, 'Merged Sheet');
 
     const outputFilePath = 'merged.xlsx';
@@ -30,6 +38,21 @@ async function mergeSheets() {
     downloadLink.href = URL.createObjectURL(new Blob([s2ab(XLSX.write(mergedWorkbook, { bookType: 'xlsx', type: 'binary' }))], { type: 'application/octet-stream' }));
     downloadLink.download = outputFilePath;
     downloadLink.click();
+  };
+
+  updateProgress(0);
+}
+
+reader.onerror = function (e) {
+    alert('Error occurred while reading the file');
+    // Reset progress on error
+    updateProgress(0);
+  };
+ reader.onprogress = function (e) {
+    if (e.lengthComputable) {
+      const progress = Math.round((e.loaded / e.total) * 100);
+      updateProgress(progress);
+    }
   };
 
   reader.readAsArrayBuffer(file);
@@ -41,4 +64,9 @@ function s2ab(s) {
   const view = new Uint8Array(buf);
   for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
   return buf;
+}
+function updateProgress(progress) {
+  const progressBar = document.getElementById('progressBar');
+  progressBar.style.width = progress + '%';
+  progressBar.textContent = progress + '%';
 }
